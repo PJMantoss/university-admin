5ac14026e3b0e4ef0ddf90a5153ae22b3053474e
pjmantoss <joelptoss@gmail.com>
2020-12-21 13:48:27 +0100

    add dependencies(styled-components,react-router-dom,normalize.css)

---
+++ src/components/header/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Container } from './styles/header';
+
+export default function Header({ children, ...restProps }){
+    return <Container {...restProps}>{children}</Container>
+}
