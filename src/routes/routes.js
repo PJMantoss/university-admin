1a2a7fad6766aba5387862a7afaa91c7d87ca274
pjmantoss <joelptoss@gmail.com>
2020-12-21 14:15:51 +0100

    add routes

---
+++ src/routes/routes.js
@@ -0,0 +1,6 @@
+export const HOME = "/";
+export const DASHBOARD = "/dashboard";
+export const STUDENTS = "/students";
+export const STUDENT = "/student";
+export const SIGN_UP = "/signup";
+export const SIGN_IN = "/signin";
