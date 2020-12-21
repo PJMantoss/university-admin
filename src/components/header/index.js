fa0fb4123235e271d042af93cfc03302fc30caec
pjmantoss <joelptoss@gmail.com>
2020-12-21 14:01:44 +0100

    build header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,33 @@
 import React from 'react';
-import { Container } from './styles/header';
+import { 
+    Background, 
+    Container, 
+    Group, 
+    Link, 
+    Text, 
+    Profile 
+} from './styles/header';
 
-export default function Header({ children, ...restProps }){
+export default function Header({ bg=true, children, ...restProps }){
+    return <Background {...restProps}>{children}</Background>
+}
+
+Header.Frame = function HeaderFrame({ children, ...restProps }){
     return <Container {...restProps}>{children}</Container>
+}
+
+Header.Group = function HeaderGroup({ children, ...restProps }){
+    return <Group {...restProps}>{children}</Group>
+}
+
+Header.Link = function HeaderLink({ children, ...restProps }){
+    return <Link {...restProps}>{children}</Link>
+}
+
+Header.Text = function HeaderText({ children, ...restProps }){
+    return <Text {...restProps}>{children}</Text>
+}
+
+Header.Profile = function HeaderProfile({ children, ...restProps }){
+    return <Profile {...restProps}>{children}</Profile>
 }
---
+++ src/components/header/styles/header.js
@@ -0,0 +1,2 @@
+import styled from 'styled-components';
+import { Link as MainRouterLink } from 'react-router-dom';
