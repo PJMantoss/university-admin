64b362a06b74516cc33c1ebdb3af272a4f0c4d90
pjmantoss <joelptoss@gmail.com>
2020-12-21 15:16:50 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Home, Dashboard, Signin, Signup} from './pages';
+import * as ROUTES from './routes/routes';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Route>
+      
+    </Route>
   );
 }
 
