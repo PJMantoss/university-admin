666265ab8cbe38079358b60edbe6b927dd734ca8
pjmantoss <joelptoss@gmail.com>
2020-12-21 14:11:25 +0100

    build header container

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import Header from '../components/header';
+
+export function HeaderContainer({ children }){
+    return(
+        <Header>
+            <Header.Frame>
+                <Header.Logo></Header.Logo>
+                <Header.ButtonLink></Header.ButtonLink>
+            </Header.Frame>
+            {children}
+        </Header>
+    )
+}
