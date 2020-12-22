81de977b6782e7ed59681c53daab32637a525380
pjmantoss <joelptoss@gmail.com>
2020-12-22 13:10:11 +0100

    update Signin page

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -4,3 +4,17 @@ import { HeaderContainer } from '../containers/Header';
 import { FooterContainer } from '../containers/Footer';
 import * as ROUTES from '../constants/routes';
 
+export default function Signin(){
+
+    //initialize state values
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSignin = e => {
+        e.preventDefault();
+        //call in firebase to handle authentication
+    }
+    
+    return()
+}
