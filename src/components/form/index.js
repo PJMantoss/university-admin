5722dff011f215b9b999a158934acaa0ee474d1e
pjmantoss <joelptoss@gmail.com>
2020-12-22 13:01:32 +0100

    update Form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,4 +1,38 @@
 import React from 'react';
 import { Container } from './styles/form';
 
-export default function Form({}){}
+export default function Form({ children, ...restProps }){
+    return <Container {...restProps}>{children}</Container>
+}
+
+Form.Input = function FormInput({ ...restProps }){
+    return <Input {...restProps}/>
+}
+
+Form.Title = function FormTitle({ children, ...restProps }){
+    return <Title {...restProps}>{ children }</Title>
+}
+
+Form.Text = function FormText({ children, ...restProps }){
+    return <Text {...restProps}>{ children }</Text>
+}
+
+Form.TextSmall = function FormTextSmall({ children, ...restProps }){
+    return <TextSmall {...restProps}>{ children }</TextSmall>
+}
+
+Form.Base = function FormBase({ children, ...restProps }){
+    return <Base {...restProps}>{ children }</Base>
+}
+
+Form.Link = function FormLink({ children, ...restProps }){
+    return <Link {...restProps}>{ children }</Link>
+}
+
+Form.Error = function FormError({ children, ...restProps }){
+    return <Error {...restProps}>{ children }</Error>
+}
+
+Form.Submit = function FormSubmit({ children, ...restProps }){
+    return <Submit {...restProps}>{ children }</Submit>
+}
