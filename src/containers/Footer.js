dc1bd7adc118a24494912e762b894711c9f214d8
pjmantoss <joelptoss@gmail.com>
2020-12-22 11:57:48 +0100

    update footer

---
+++ src/containers/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Footer } from '../components';
+
+export function FooterContainer(){
+    return(
+        <Footer>
+            <Footer.Title>Admin University</Footer.Title>
+            <Footer.Break/>
+            <Footer.Row></Footer.Row>
+            <Footer.Break/>
+            <Footer.Text></Footer.Text>
+        </Footer>
+    )
+}
