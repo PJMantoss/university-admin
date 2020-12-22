eadc0c851a5496bc1b7b83bb4964584a3a8eef84
pjmantoss <joelptoss@gmail.com>
2020-12-22 11:40:39 +0100

    update footer

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -7,7 +7,17 @@ export const Container = styled.div`
     padding: 70px 56px;
     margin: auto;
 
-    @media(max-width: 600px){
+    @media(max-width: 900px){
         padding: 60px 30px;
+    }
+`;
+
+export const Row = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+    grid-gap: 15px;
+
+    @media(max-width: 900px){
+        grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
     }
 `;
