e324cfda9ea4aeff35fc204096f1327ca8565f9e
pjmantoss <joelptoss@gmail.com>
2020-12-23 15:30:50 +0100

    update course component

--- src/components/courses/index.js
+++ src/components/courses/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import courseData from '../../data/courses.json';
 import './styles.css'
 
 export default function Courses(){
@@ -8,5 +9,47 @@ export default function Courses(){
             <AddCourse />
             <RemoveCourse />
         </div>
+    )
+}
+
+const CourseList = () => {
+    return(
+        <div className="container">
+            <ul className="course-list">
+                {Object.keys(courseData).map(course => (
+                    <li className="course-item">{courseData[course]}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+const AddCourse = () => {
+    const ref = useRef(null);
+
+    const createCourse = (e) => {
+        e.preventDefault();
+
+        //Get the course name from the form
+        let course = ref.courseName.value
+
+        //call the addCourse method
+        if(courseData.length > 0){
+            AddCourse(course);
+        }
+
+        //reset form
+        ref.courseForm.reset();
+    }
+
+    return(
+        <form ref="courseForm" onSubmit={createCourse}>
+            <div className="form-group">
+                <label for="courseItem">
+                    <input type="text" id="courseItem" className="form-control" ref="courseName" />
+                </label>
+            </div>
+            <button type="submit" className="submit-btn">Add Course</button>
+        </form>
     )
 }
