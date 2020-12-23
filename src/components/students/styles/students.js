2a3ffb147568510f7c90898338fa9a5799ed12d7
pjmantoss <joelptoss@gmail.com>
2020-12-23 13:36:20 +0100

    update studentList component styling

--- src/components/students/styles/students.js
+++ src/components/students/styles/students.js
@@ -4,3 +4,16 @@ export const Container = styled.section`
     display: flex;
     border-bottom: 8px solid #222;
 `;
+
+export const Inner = styled.div`
+    display: flex;
+    padding: 70px 45px;
+    flex-direction: column;
+    max-width: 815px;
+    margin: auto;
+`;
+
+export const Frame = styled.div`
+    max-width: 1200px;
+    margin-bottom: 10px;
+`;
